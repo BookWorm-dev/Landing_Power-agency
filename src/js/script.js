a87307f9139186aa88fa2646f01d15d682c29d17
Kristina Gritsenko <kgritsenko.2017@gmail.com>
2024-11-23 21:45:00 +0200

    fied:Swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,16 @@
 const body = document.querySelector('body');
 const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	direction: 'vertical',
+	slidesPerView: 1,
+	spaceBetween: 10,
 	loop: true,
-
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-	},
-
-	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
-
-	// And if we need scrollbar
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
 	scrollbar: {
 		el: '.swiper-scrollbar',
 	},
