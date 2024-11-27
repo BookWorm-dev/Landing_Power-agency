d36c5e11b0cadff19ce44f081ee96591b003af83
Kristina Gritsenko <kgritsenko.2017@gmail.com>
2024-11-27 23:37:25 +0200

    fied:final

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,105 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
 const sass = require('gulp-dart-sass');
-const fileinclude = require('gulp-file-include');
-const cfg = require('./package.json').config;
-const csso = require('gulp-csso');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-const terser = require('gulp-terser');
-const browserslist = ['> 1%, last 3 versions, not dead'];
+(fileinclude = require('gulp-file-include')),
+	(cfg = require('./package.json').config),
+	(csso = require('gulp-csso')),
+	(concat = require('gulp-concat')),
+	(autoprefixer = require('gulp-autoprefixer')),
+	(browserSync = require('browser-sync').create()),
+	(terser = require('gulp-terser')),
+	(browserslist = ['> 1%, last 3 versions, not dead']);
 
 function html() {
-  return src([cfg.srcDir + '/*.html'])
-    .pipe(fileinclude({ prefix: '@@', basepath: '@file' }))
-    .pipe(dest(cfg.outputDir))
-    .pipe(browserSync.stream({ once: true }));
+	return src([cfg.srcDir + '/*.html'])
+		.pipe(
+			fileinclude({
+				prefix: '@@',
+				basepath: '@file',
+			}),
+		)
+		.pipe(dest(cfg.outputDir))
+		.pipe(browserSync.stream({ once: true }));
 }
 
 function styles() {
-  return src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
-    .pipe(sass({ outputStyle: 'expanded', silenceDeprecations: ['legacy-js-api'] }).on('error', sass.logError))
-    .pipe(autoprefixer({ overrideBrowserslist: browserslist }))
-    .pipe(dest(cfg.outputDir + 'css', { sourcemaps: '.' }))
-    .pipe(browserSync.stream({ once: true }));
+	return (
+		src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: true })
+			.pipe(
+				sass({
+					outputStyle: 'expanded', // expanded/compressed
+					silenceDeprecations: ['legacy-js-api'], // Disable old API warnings
+				}).on('error', sass.logError),
+			)
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: browserslist,
+				}),
+			)
+	
+			.pipe(dest(cfg.outputDir + 'css', { sourcemaps: '.' }))
+			.pipe(browserSync.stream({ once: true }))
+	);
 }
 
 function stylesMin() {
-  return src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: false })
-    .pipe(sass({ outputStyle: 'compressed', silenceDeprecations: ['legacy-js-api'] }).on('error', sass.logError))
-    .pipe(autoprefixer({ overrideBrowserslist: browserslist }))
-    .pipe(dest(cfg.outputDir + 'css'));
+	return (
+		src(cfg.srcDir + 'scss/**/*.{scss,sass}', { sourcemaps: false })
+			.pipe(
+				sass({
+					outputStyle: 'compressed', // expanded/compressed
+					silenceDeprecations: ['legacy-js-api'], // Disable old API warnings
+				}).on('error', sass.logError),
+			)
+			.pipe(
+				autoprefixer({
+					overrideBrowserslist: browserslist,
+				}),
+			)
+			// .pipe(csso())
+			.pipe(dest(cfg.outputDir + 'css'))
+	);
 }
 
 function scripts() {
-  return src(cfg.srcDir + 'js/**/*.js')
-    .pipe(concat('script.min.js'))
-    .pipe(terser())
-    .pipe(dest(cfg.outputDir + 'js'))
-    .pipe(browserSync.stream({ once: true }));
+	return src(cfg.srcDir + 'js/**/*.js')
+		.pipe(concat('script.min.js'))
+		.pipe(terser())
+		.pipe(dest(cfg.outputDir + 'js'))
+		.pipe(browserSync.stream({ once: true }));
 }
 
 function imageSync() {
-  return src('src/imgs/**/*', { encoding: false })
-    .pipe(dest('app/imgs'))
-    .pipe(browserSync.stream({ once: true }));
+	return src('src/imgs/**/*', { encoding: false })
+		.pipe(dest('app/imgs'))
+		.pipe(browserSync.stream({ once: true }));
 }
 
-function browsersync(done) {
-  browserSync.init({
-    server: { baseDir: cfg.outputDir },
-  });
-  done(); // Уведомляем Gulp о завершении задачи
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: cfg.outputDir,
+		},
+	});
 }
 
 function watching() {
-  watch([cfg.srcDir + 'scss/**/*.scss'], styles);
-  watch([cfg.srcDir + 'js/**/*.js'], scripts);
-  watch([cfg.srcDir + '/**/*.html'], html);
-  watch([cfg.srcDir + 'imgs/**/*'], imageSync);
+	watch([cfg.srcDir + 'scss/**/*.scss'], styles);
+	watch([cfg.srcDir + 'js/**/*.js'], scripts);
+	watch([cfg.srcDir + '/**/*.html'], html);
+	watch([cfg.srcDir + 'imgs/**/*'], imageSync);
 }
 
 async function loadPrettier() {
-  const prettier = await import('gulp-prettier');
-  return prettier.default;
+	const prettier = await import('gulp-prettier');
+	return prettier.default;
 }
 
 async function pretty() {
-  const prettier = await loadPrettier();
-  return src(['src/**/*', '!src/imgs/**/*']).pipe(prettier()).pipe(dest('src'));
+	const prettier = await loadPrettier();
+	return src(['src/**/*', '!src/imgs/**/*']).pipe(prettier()).pipe(dest('src'));
 }
 
 exports.format = pretty;
 exports.cssmin = stylesMin;
-exports.default = series(parallel(html, styles, scripts, imageSync), browsersync, watching);
+exports.default = parallel(html, styles, scripts, imageSync, watching, browsersync);
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
 const body = document.querySelector('body');
 document.addEventListener('DOMContentLoaded', () => {
     const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3, // Количество видимых слайдов
-        spaceBetween: 30, // Расстояние между слайдами
-        loop: true, // Зацикливание слайдов
+        slidesPerView: 3, 
+        spaceBetween: 30, 
+        loop: true, 
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -13,17 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
             clickable: true,
         },
         breakpoints: {
-            // Для мобильных устройств
             640: {
                 slidesPerView: 1,
                 spaceBetween: 20,
             },
-            // Для планшетов
             768: {
                 slidesPerView: 2,
                 spaceBetween: 20,
             },
-            // Для десктопов
             1024: {
                 slidesPerView: 3,
                 spaceBetween: 30,
