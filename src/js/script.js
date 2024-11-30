d24ae83608a789cdf8418a5e0fab468c8c0442b1
Kristina <kgritsenko.2017@gmail.com>
2024-11-30 11:09:58 +0200

    fied:update_comments_section

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,11 @@
 const body = document.querySelector('body');
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper-container', {
-        slidesPerView: 3, 
-        spaceBetween: 30, 
-        loop: true, 
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        breakpoints: {
-            640: {
-                slidesPerView: 1,
-                spaceBetween: 20,
-            },
-            768: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-            },
-        },
-    });
+const swiperTwo = new Swiper(".swiper_2",{
+    slidesPerView: 5,
+    loop: !0
+})
+  , swiperOne = new Swiper(".swiper_1",{
+    initialSlide: 2,
+    thumbs: {
+        swiper: swiperTwo
+    }
 });
-
