fd48ba063b455c75dd87d21db613b26f671db4a5
Kristina <kgritsenko.2017@gmail.com>
2024-11-30 13:04:57 +0200

    fied:finalize_conmpanies

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,26 @@ const swiperTwo = new Swiper(".swiper_2",{
     slidesPerView: 5,
     loop: !0
 })
-  , swiperOne = new Swiper(".swiper_1",{
-    initialSlide: 2,
-    thumbs: {
-        swiper: swiperTwo
-    }
+, swiperOne = new Swiper(".swiper_1",{
+  initialSlide: 2,
+  thumbs: {
+      swiper: swiperTwo
+  }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const swiperFour = new Swiper(".swiper_4", {
+        slidesPerView: 5,
+        loop: true,
+        spaceBetween: 20,
+    });
+
+    const swiperThree = new Swiper(".swiper_3", {
+        initialSlide: 0,
+        thumbs: {
+            swiper: swiperFour,
+        },
+    });
+});
+
