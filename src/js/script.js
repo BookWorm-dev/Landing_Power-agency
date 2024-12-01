0981d11c89025de205899b80b3b01701273fc445
Kristina <kgritsenko.2017@gmail.com>
2024-12-01 18:26:55 +0200

    swiper add

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,33 @@
 const body = document.querySelector('body');
-const swiperTwo = new Swiper(".swiper_2",{
+const swiperTwo = new Swiper(".swiper_2", {
     slidesPerView: 5,
     loop: !0
 })
-, swiperOne = new Swiper(".swiper_1",{
-  initialSlide: 2,
-  thumbs: {
-      swiper: swiperTwo
-  }
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const swiperFour = new Swiper(".swiper_4", {
-        slidesPerView: 5,
-        loop: true,
-        spaceBetween: 20,
-    });
-
-    const swiperThree = new Swiper(".swiper_3", {
-        initialSlide: 0,
+    , swiperOne = new Swiper(".swiper_1", {
+        initialSlide: 2,
         thumbs: {
-            swiper: swiperFour,
-        },
+            swiper: swiperTwo
+        }
     });
+
+
+const swiper3 = new Swiper('.swiper_3', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+
 });
 
+const swiper4 = new Swiper('.swiper_4', {
+    loop: true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+});
