6b55ba0fc419c9e1e72f3acbb08b752ca7f3dc7a
Kristina <kgritsenko.2017@gmail.com>
2024-12-12 23:58:10 +0200

    fied: mobile menu and fix footer, and fix bth hero

--- src/js/script.js
+++ src/js/script.js
@@ -85,3 +85,19 @@ document.querySelector(".block") && (document.querySelector(".block").innerHTML 
     });
 });
  */
+
+const menuButton = document.querySelector(".menu_button");
+const menuCloseButton = document.querySelector(".nav__close");
+const menu = document.querySelector(".nav");
+const body = document.querySelector(".body");
+
+menuButton.addEventListener("click", function () {
+    menu.classList.add("active");
+    body.classList.add("body--overflow_hidden");
+});
+
+menuCloseButton.addEventListener("click", function () {
+    menu.classList.remove("active");
+    body.classList.remove("body--overflow_hidden");
+});
+
